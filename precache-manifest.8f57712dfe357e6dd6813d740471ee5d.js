e01291e34325da1df9ab6eba5316fec9743776f2
pavankumar kalluri <jyotshna12121999@gmail.com>
2020-07-14 23:55:12 +0530

    Updates

---
+++ precache-manifest.8f57712dfe357e6dd6813d740471ee5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61577f2c76bdf1877b117ed8f005c889",
+    "url": "/react.hello/index.html"
+  },
+  {
+    "revision": "2db5dd3062c6bce3634d",
+    "url": "/react.hello/static/css/main.919658d8.chunk.css"
+  },
+  {
+    "revision": "28b3e627909b01113851",
+    "url": "/react.hello/static/js/2.07f71799.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react.hello/static/js/2.07f71799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db5dd3062c6bce3634d",
+    "url": "/react.hello/static/js/main.a9fa6a0f.chunk.js"
+  },
+  {
+    "revision": "2e3e8e8e45019fb7c3e8",
+    "url": "/react.hello/static/js/runtime-main.5613f78c.js"
+  },
+  {
+    "revision": "4948508ccfd3552d72bc0783dfe5502f",
+    "url": "/react.hello/static/media/delivery-man.4948508c.svg"
+  }
+]);
