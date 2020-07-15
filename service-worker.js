202d2219dc5b035c490efe44d237e095fd8de95b
pavankumar kalluri <jyotshna12121999@gmail.com>
2020-07-15 18:07:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/*%20https://jyotshna1212.github.io/react.hello%20*/precache-manifest.1e151c77288a31977361c9622cc810c7.js"
+  "/react.hello/precache-manifest.3c982b9e8b3103cc1dd2da5e311a0de6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/*%20https://jyotshna1212.github.io/react.hello%20*/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react.hello/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
